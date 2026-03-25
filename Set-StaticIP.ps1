f391d3e389ed9b1d118de342e71db8f3e3d9e2ea
Zbargstadt <151299775+Zbargstadt@users.noreply.github.com>
2026-03-25 14:52:33 -0500

    Update and rename scripts/scripts1 to Set-StaticIP.ps1

---
+++ Set-StaticIP.ps1
@@ -0,0 +1,10 @@
+# This power script will set the configure the static IP and DNS for the Lab Domain Controller (DC01).
+
+# Identifies the active network adapter
+$Interface = (Get-NetAdapter | Where-Object Status -eq "Up" | Select-Object -First 1).Name
+
+# Sets the static IP
+New-NetIPAddress -InterfaceAlias $Interface -IPAddress 192.168.10.10 -PrefixLength 24 -DefaultGateway 192.168.10.2
+
+# Sets local DNS loopback
+Set-DnsClientServerAddress -InterfaceAlias $Interface -ServerAddresses 127.0.0.1
